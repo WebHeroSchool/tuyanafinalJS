b960ea960928e461c6e5fc2609a0528582554ee9
Tuyana Tsydenova <tsidenovatb@gmail.com>
2020-09-25 23:47:17 +0400

    saveall

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let tableMedium = document.getElementById('tablemedium');
 let tableHard = document.getElementById('tablehard');
 let firstPage = document.getElementById('firstpage');
 let levels = document.getElementsByName('level');
-let card = document.querySelectorAll('.card');
-let frontCard = document.querySelectorAll('.frontcard');
-let backCard = document.querySelectorAll('.backcard');
+let card = document.querySelectorAll('.section__card');
+let frontCard = document.querySelectorAll('.card__front');
+let backCard = document.querySelectorAll('.card__back');
 
 /*переход со стартовой страницы к столам в зависимости от сложности*/
 
@@ -14,48 +14,49 @@ startButton.addEventListener('click', event => {
 	for (let i = 0; i < levels.length; i++) {
 		if (levels[0].checked) {  /*легкий уровень*/
 			tableLight.classList.remove('section__levellight');
-			tableLight.classList.add('table');
+			tableLight.classList.add('table__style');
 			firstPage.classList.add('section__disappear');
-
-			card.forEach((item, m) => {
-				card[m].onmouseover = () => frontCard[m].classList.add('card__hover');
-  				card[m].onmouseout = () => frontCard[m].classList.remove('card__hover');
-			})
-			
-
 			let allBacksideImages  = ['./cardbug.png', './cardgameover.png', './cardgameover.png'];
-			let backsideImgLight = document.querySelectorAll('.backside__light');
+			let backsideImgLight = document.querySelectorAll('.back__img_light');
 
-			function shuffleImages (arr) {
-				let j, temp;
-                    for (let i = arr.length - 1; i>0; i--) {
-                    j = Math.floor(Math.random() * (i+1));
-                    temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
-                    }
-                    return arr;
+
+			function prepareTable () {
+				card.forEach((item, m) => {
+					card[m].onmouseover = () => frontCard[m].classList.add('card__hover');
+	  				card[m].onmouseout = () => frontCard[m].classList.remove('card__hover');
+				})
+				
+				function shuffleImages (arr) {
+					let j, temp;
+	                    for (let i = arr.length - 1; i>0; i--) {
+	                    j = Math.floor(Math.random() * (i+1));
+	                    temp = arr[j];
+	                    arr[j] = arr[i];
+	                    arr[i] = temp;
+	                    }
+	                    return arr;
+				}
+				let newArray = shuffleImages(allBacksideImages);
+
+				backsideImgLight.forEach((elem, i) =>{
+					backsideImgLight[i].src = newArray[i];
+				})
 			}
-			let newArray = shuffleImages(allBacksideImages);
-
-			backsideImgLight.forEach((elem, i) =>{
-				backsideImgLight[i].src = newArray[i];
-			})
-
-			
+						
 		} 
 		else if(levels[1].checked) {  /*средний уровень*/
 			tableMedium.classList.remove('section__levelmedium');
-			tableMedium.classList.add('table');
+			tableMedium.classList.add('table__style');
 			firstPage.classList.add('section__disappear');
+			let allBacksideImages  = ['./cardbug.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png'];
+			let backsideImgMedium = document.querySelectorAll('.back__img_medium');
+
+			
 
 			card.forEach((item, m) => {
 				card[m].onmouseover = () => frontCard[m].classList.add('card__hover');
   				card[m].onmouseout = () => frontCard[m].classList.remove('card__hover');
 			})
-
-			let allBacksideImages  = ['./cardbug.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png'];
-			let backsideImgMedium = document.querySelectorAll('.backside__medium');
 
 			function shuffleImages (arr) {
 				let j, temp;
@@ -75,16 +76,18 @@ startButton.addEventListener('click', event => {
 		} 
 		else if (levels[2].checked) { /*сложный уровень*/
 			tableHard.classList.remove('section__levelhard');
-			tableHard.classList.add('table');
+			tableHard.classList.add('table__style');
 			firstPage.classList.add('section__disappear');
+			let allBacksideImages  = ['./cardbug.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png'];
+			let backsideImgHard = document.querySelectorAll('.back__img_hard');
+			
+			
 
+			
 			card.forEach((item, m) => {
 				card[m].onmouseover = () => frontCard[m].classList.add('card__hover');
   				card[m].onmouseout = () => frontCard[m].classList.remove('card__hover');
 			})
-
-			let allBacksideImages  = ['./cardbug.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png', './cardgameover.png'];
-			let backsideImgHard = document.querySelectorAll('.backside__hard');
 
 			function shuffleImages (arr) {
 				let j, temp;
@@ -111,9 +114,9 @@ startButton.addEventListener('click', event => {
 
 card.forEach((item, n) => { 
 	card[n].addEventListener('click', event => {
-        card[n].classList.add('flip');
-        backCard[n].classList.remove('backcard');
-        frontCard[n].classList.add('backcard');
+        card[n].classList.add('card__flip');
+        backCard[n].classList.remove('card__back');
+        frontCard[n].classList.add('card__back');
         element.className = 'newelement';
 		document.body.appendChild(element);
 		element.addEventListener('click', click);
@@ -125,17 +128,17 @@ let element = document.createElement('div');
 const click = () => {
 	firstPage.classList.remove('section__disappear');
 	tableLight.classList.add('section__levellight');
-	tableLight.classList.remove('table');
+	tableLight.classList.remove('table__style');
 	tableMedium.classList.add('section__levelmedium');
-	tableMedium.classList.remove('table');
+	tableMedium.classList.remove('table__style');
 	tableHard.classList.add('section__levelhard'); 
-	tableHard.classList.remove('table');
+	tableHard.classList.remove('table__style');
 	document.body.removeChild(element);
 
 		card.forEach((item, index) => {
-			card[index].classList.remove('flip');
-			backCard[index].classList.add('backcard');
-			frontCard[index].classList.remove('backcard');
+			card[index].classList.remove('card__flip');
+			backCard[index].classList.add('card__back');
+			frontCard[index].classList.remove('card__back');
 			card[index].removeEventListener('click', click);
 		})
 }
